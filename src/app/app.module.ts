39b68e744126c53f312338749c3539b11de4a3ca
gudpick <vinusagar@live.in>
2019-02-28 18:34:15 +0530

    Initial Setup For an NgRx project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AuthModule} from './auth-module/auth.module';
+import {AuthRoutingModule} from './auth-module/auth-routing';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+      declarations: [
+            AppComponent
+      ],
+      imports: [
+            BrowserModule,
+            BrowserAnimationsModule,
+            AuthModule,
+            AuthRoutingModule
+      ],
+      providers: [],
+      bootstrap: [AppComponent]
 })
 export class AppModule { }
